40d9a888ac606549bb0cdafbef19a89347fc8891
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-16 05:49:03 -0500

    fix issue

--- server.js
+++ server.js
@@ -1,32 +1,61 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-
-users = [];
-connnections = [];
-
-server.listen(process.env.PORT || 3000);
-console.log("Server Running...")
-
-
-
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+users = []; 
+connections = []; 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html'); 
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connnection', function(socket){
-	connnections.push(socket);
-	console.log("connected %s sockets connected", connnections.length);
+io.on('connection', function(socket){
+  	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
 
-	//Disconnect
-	sockets.on('disconnect', function(data){
-		connnections.splice(connnections.indexOf(socket), 1);
-		console.log('Disconnected %s sockets connected', connnections.length);
-	});	
+	//Disconnect 
+	socket.on('disconnect', function(data){
+    	connections.splice(connections.indexOf(socket), 1);
+    	console.log('Disconnected: %s sockets connected', connections.length);
+  	});
 
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data});
-	});
+  	//Send Message 
+  	socket.on('send message', function(data){
+		//console.log("It worked"); 
+		//console.log(data); 
+		io.sockets.emit('new message', {msg: data, user: socket.username}); 
+	}); 
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+// console.log('Server running...'); 
+
+
+
+
+// io.sockets.on('connnection', function(socket){
+	// connections.push(socket);
+	// console.log('Connected: %s sockets connected', connections.length);
+
+// 	socket.on('disconnect', function(data){
+		// connections.splice(connections.indexOf(socket), 1);
+  //   	console.log('Disconnected: %s sockets connected', connections.length);
+//   });
+
+	// //Disconnect 
+	// socket.on('disconnect', function(data){ 
+	// 	//users.splice(users.indexOf(socket.username), 1); 
+	// 	//updateUsernames(); 
+	// 	connections.splice(connections.indexOf(socket), 1); 
+	// 	console.log('Disconnected: %s sockets connected', connections.length); 
+	// }); 
+	// //Send Message 
+	// socket.on('send message', function(data){
+	// 	console.log("It worked"); 
+	// 	console.log(data); 
+	// 	io.sockets.emit('new message', {msg: data, user: socket.username}); 
+	// }); 
 	
-});
+// });
